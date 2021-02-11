d12e064f2c02a2b0b15e5c884fc4467815622824
Bankole Jamgbadi <bjamgbadi@gmail.com>
2021-02-11 14:06:31 +0100

    Initial Commit

---
+++ Shared/newCurioApp.swift
@@ -0,0 +1,17 @@
+//
+//  newCurioApp.swift
+//  Shared
+//
+//  Created by user on 2021-02-11.
+//
+
+import SwiftUI
+
+@main
+struct newCurioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
